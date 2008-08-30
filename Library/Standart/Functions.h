3e39200787a115014bafe0b654cdd672adf57c43
wallach2008 <wallach2008>
2008-08-30 20:43:28 +0000

    minor

--- Library/Standart/Functions.h
+++ Library/Standart/Functions.h
@@ -1,8 +1,8 @@
-//---------------------------------------------------------------------------
-
+﻿//---------------------------------------------------------------------------
 #ifndef FunctionsH
 #define FunctionsH
 //---------------------------------------------------------------------------
 int WritePointers(TStream& stream,TList& list);//returm number of pointer was writed
 int ReadPointers(TStream& i_stream,TList& o_list);//return number of pointers was read
 #endif
+
