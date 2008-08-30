27b310f5445c01874a73caa9bcfc257b4c62414e
wallach2008 <wallach2008>
2008-08-30 20:57:12 +0000

    catch std::exception

--- Cheater/HCheater.cpp
+++ Cheater/HCheater.cpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
-
 #include <vcl.h>
 #pragma hdrstop
+#include <stdexcept>
 USEFORM("Main.cpp", Form1);
 USEFORM("..\Library\ProcessChoiceFrame.cpp", ProcessChoice1);
 //---------------------------------------------------------------------------
@@ -9,14 +9,20 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
         try
         {
-                 Application->Initialize();
-                 Application->CreateForm(__classid(TForm1), &Form1);
-		Application->Run();
+           Application->Initialize();
+           Application->CreateForm(__classid(TForm1), &Form1);
+           Application->Run();
         }
         catch (Exception &exception)
         {
                  Application->ShowException(&exception);
         }
+        catch(std::exception &exception)
+        {
+            Application->MessageBox( exception.what(), "std::exception");
+        }
+
         return 0;
 }
 //---------------------------------------------------------------------------
+
