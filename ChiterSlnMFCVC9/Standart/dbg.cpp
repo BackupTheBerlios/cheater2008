d7fd35e413b59503d90fbd2e676c16d932963751
wallach2008 <wallach2008>
2010-01-23 16:13:30 +0000

    debug streaming

---
+++ ChiterSlnMFCVC9/Standart/dbg.cpp
@@ -0,0 +1,10 @@
+﻿//---------------------------------------------------------------------------
+#include "stdafx.h"
+#pragma hdrstop
+#include "dbg.h"
+
+void dprintf(const std::string& i_str)
+{
+  OutputDebugString(CString(i_str.c_str()) );
+}
+
---
+++ ChiterSlnMFCVC9/Standart/dbg.h
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef DBG_H
+#define DBG_H
+#include <sstream>
+#include <string>
+#include "Standart/StandartFwd.h"
+//---------------------------------------------------------------------------
+STANDART_EXPORT_FUN(void) dprintf(const std::string& );
+
+#define DPRINTF(m)\
+  { \
+  std::stringstream msg; \
+  msg << __FILE__ << "(" << __LINE__ << ") " << __FUNCTION__ << std::endl; \
+  msg << m; \
+  dprintf(msg.str()); \
+  }  
+
+#endif
+
